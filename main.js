e7ac3369316214cecb1bbebc8acbf8385367c36e
CJ <ngassajason@yahoo.fr>
2021-06-12 13:37:27 +0100

    first tuto

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            description: `I love ${product}`
+        }
+    },
+
+})
+
+
+// createApp expects an option object to configure the initial vue app.
+// This object expects a data attribute which is a fonction
+// The data function equally creates attributes of the vue app.
